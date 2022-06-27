f0564b9713e351ebcca1e750953e4adeb530c08f
Nonnok <donntenn0917@icloud.com>
2022-06-27 16:57:28 +0900

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// FadeUp Animation
+function fadeAnime() {
+  $('.fadeUpTrigger').each(function() {
+    var elemPos = $(this).offset() . top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeUp');
+    } else {
+      $(this).removeClass('fadeUp');
+    }
+  });
+}
+
+$(window).scroll(function() {
+  fadeAnime();
+});
+
+// PageTopLink Animation
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 100) {
+    $('#page-top').removeClass('DownMove');
+    $('#page-top').addClass('UpMove');
+  } else {
+    if($('#page-top').hasClass('UpMove')){
+      $('#page-top').removeClass('UpMove');
+      $('#page-top').addClass('DownMove');
+    }
+  }
+}
+
+$(window).scroll(function () {
+  PageTopAnime();
+});
+
+$('#page-top').click(function () {
+  var scroll = $(window).scrollTop();
+  if(scroll > 0) {
+  $(this).addClass('floatAnime');
+    $('body,html').animate({
+        scrollTop: 0
+    }, 2000,function() {
+        $('#page-top').removeClass('floatAnime');
+    }); 
+  }
+  return false;
+});
+
+// Slider Animation
+$('#slider').vegas({
+  slides: [
+    { src: 'img/artiodactyls-heel-after.png' },
+    { src: 'img/jump-after.png' },
+    { src: 'img/posing-after.png' },
+    { src: 'img/artiodactyls1-after.png'},
+    { src: 'img/artiodactyls5-after.png' },
+    { src: 'img/deformation-after.png' },
+    { src: 'img/Child-after.png' },
+    { src: 'img/Chine-after.png' },
+    { src: 'img/artiodactyls3-after.png' },
+    { src: 'img/sit-down-after.png' },
+    { src: 'img/fullface-after.png' },
+    { src: 'img/artiodactyls6-after.png' },
+    { src: 'img/artiodactyls4-after.png' },
+  ],
+  overlay: true,
+  transition: 'fade2',
+  transitionDuration: 2000,
+  delay: 5000,
+  animationDuration: 20000,
+  animation: 'random',
+});
