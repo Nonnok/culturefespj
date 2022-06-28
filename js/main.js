117e44bdd7b17c2b4917cdb73e3614f42c960874
Nonnok <donntenn0917@icloud.com>
2022-06-28 14:52:30 +0900

    add typinganimation

--- js/main.js
+++ js/main.js
@@ -71,3 +71,42 @@ $('#slider').vegas({
   animationDuration: 20000,
   animation: 'random',
 });
+
+// Typing Animation
+function TextTypingAnime() {
+	$('.TextTyping').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var thisChild = "";
+		if (scroll >= elemPos - windowHeight) {
+			thisChild = $(this).children();
+			thisChild.each(function (i) {
+				var time = 100;
+				$(this).delay(time * i).fadeIn(time);
+			});
+		} else {
+			thisChild = $(this).children();
+			thisChild.each(function () {
+				$(this).stop();
+				$(this).css("display", "none");
+			});
+		}
+	});
+}
+$(window).on('load', function () {
+	var element = $(".TextTyping");
+	element.each(function () {
+		var text = $(this).html();
+		var textbox = "";
+		text.split('').forEach(function (t) {
+			if (t !== " ") {
+				textbox += '<span>' + t + '</span>';
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+	});
+	TextTypingAnime();
+});
