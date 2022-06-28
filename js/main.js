987c20fbc478d1622efd6052fa5b8c56bf2fcd1b
Nonnok <donntenn0917@icloud.com>
2022-06-28 16:01:37 +0900

    add loading animation

--- js/main.js
+++ js/main.js
@@ -110,3 +110,34 @@ $(window).on('load', function () {
 	});
 	TextTypingAnime();
 });
+
+// Loading Animation
+var bar = new ProgressBar.Line(splash_text, {
+  easing: 'easeInOut',
+  duration: 0.2,
+  color: '#555',
+  trailWidth: 0.2,
+  trailColor: '#bbb',
+  text: {
+    style: {
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',
+      transform: 'translate(-50%, -50%)',
+      'font-size': '1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + '%');
+  }
+});
+bar.animate(1.0, function() {
+  $("#splash_text").fadeOut(10);
+  $(".loader_cover-up").addClass("coveranime");
+  $(".loader_cover-down").addClass("coveranime");
+  $("#splash").fadeOut();
+});
